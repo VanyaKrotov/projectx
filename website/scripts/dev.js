e8f2474542c3a579c2e80d6d3616677bfabed326
Ivan Krotov <vanyakrotov3059@gmail.com>
2023-02-14 10:55:37 +0300

    change: fix failure install deps for website

--- website/scripts/dev.js
+++ website/scripts/dev.js
@@ -1,13 +1,35 @@
 const esbuild = require("esbuild");
+const fs = require("fs");
+const path = require("path");
 
 const { getConfig } = require("../config/website.config");
 const { runServer } = require("../../scripts/server");
 
+const COPY_FILES = ["index.html", "favicon.ico"];
+
 esbuild
-  .context(getConfig({}))
-  .then((ctx) => ctx.watch())
+  .build({
+    watch: true,
+    ...getConfig({}),
+  })
   .catch((err) => {
     console.error(err);
     process.exit(1);
   })
-  .then(() => runServer(3000, "../website/dist"));
+  .then(() => {
+    COPY_FILES.forEach((filename) => {
+      fs.copyFile(
+        path.resolve(__dirname, `../public/${filename}`),
+        path.resolve(__dirname, `../dist/${filename}`),
+        (error) => {
+          if (error) {
+            throw new Error(`Copy file error! ${error.message}`);
+          }
+        }
+      );
+    });
+
+    return runServer(3000, "../website/dist");
+  });
+
+// esbuild.context().then((ctx) => ctx.watch());
